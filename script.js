4624e812917369c1c80acf8f5e0b01cba3e36f3b
AidenKwong <>
2021-06-08 15:01:07 +0000

    js fix

--- script.js
+++ script.js
@@ -5,20 +5,17 @@ document.getElementById('calc-btn').addEventListener('click', checkNumber)
 
 // Function determines if input is a number, if it is positive, zero or negative.
 function checkNumber () {
-
   // Determines the value of the number in the input field.
   number = document.getElementById('input').value
   number = parseInt(number)
 
   // Confirm that the input is actually a number.
   if (isNaN(number)) {
-    
     // If it isn't a number, display this message.
     document.getElementById('result').innerHTML = 'This is not a number!'
 
   // If it is a number, proceed with these steps.
   } else {
-
     // If the number is greater than 0, output this message.
     if (number > 0) {
       document.getElementById('result').innerHTML = 'The number is positive.'
